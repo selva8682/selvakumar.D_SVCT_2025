a99422e120ebe08bb9dbc0fa577c0498648fc956
selva8682 <125633576+selva8682@users.noreply.github.com>
2024-01-25 12:48:07 +0530

    Add files via upload

---
+++ 22.01.2024/22.01.2024_01_Reverse a Number.py
@@ -0,0 +1,31 @@
+#Example 2: Input: N = 234 Output: 432 Explanation: The reverse of 234 is 432
+
+#Input Format
+
+#123
+
+#Constraints
+
+#N <= 1000
+
+#Output Format
+
+#321
+
+#Sample Input 0
+
+#123
+#Sample Output 0
+
+#321
+#Sample Input 1
+
+#2341
+#Sample Output 1
+
+#1432
+
+
+
+N = input()
+print(N[::-1])
